c7bc7ab6ae78d7916bf81cb0b01df0cb4d83a551
Kacper Manterys <kacpermanterys@gmail.com>
2022-06-25 22:52:11 +0200

    Add Class Serial

--- Biblioteczka.py
+++ Biblioteczka.py
@@ -24,3 +24,24 @@ class Film:
     def __add__(self):
         f = self.odtworzenia + 1
         return self.odtworzenia + 1
+
+Karaiby = Film(tytuł="Karaiby", wydanie="2011", gatunek="thriller", odtworzenia=[300])
+Scooby_doo = Film(tytuł="Scooby_Doo", wydanie="2004", gatunek="komedia", odtworzenia=[200])
+Dom_strachu = Film(tytuł="Dom_strachu", wydanie="2018", gatunek="kryminalny", odtworzenia=[600])
+
+class Serial(Film):
+
+    def __init__(self, odcinek, sezon, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.odcinek = odcinek
+        self.sezon = sezon
+
+    def __str__(self):
+        info = self.tytuł + "\n"
+        info = self.sezon + "\n"
+        info += self.odcinek + "\n"
+        return info
+
+Rodzinka = Serial(tytuł="Rodzinka", wydanie="2003", gatunek="komedia", odtworzenia=[400], odcinek="E05", sezon="S03")
+Dexter = Serial(tytuł="Dexter", wydanie="2007", gatunek="horror", odtworzenia=[670], odcinek="E04", sezon="S09")
+Spongebob = Serial(tytuł="Spongebob", wydanie="2000", gatunek="komedia", odtworzenia=[240], odcinek="E02", sezon="S08")
