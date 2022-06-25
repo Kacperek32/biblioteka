a15897c0389f54a9d069d7b85f8196a65bbda6e0
Kacper Manterys <kacpermanterys@gmail.com>
2022-06-25 22:53:24 +0200

    Add list and sort

--- Biblioteczka.py
+++ Biblioteczka.py
@@ -45,3 +45,14 @@ class Serial(Film):
 Rodzinka = Serial(tytuł="Rodzinka", wydanie="2003", gatunek="komedia", odtworzenia=[400], odcinek="E05", sezon="S03")
 Dexter = Serial(tytuł="Dexter", wydanie="2007", gatunek="horror", odtworzenia=[670], odcinek="E04", sezon="S09")
 Spongebob = Serial(tytuł="Spongebob", wydanie="2000", gatunek="komedia", odtworzenia=[240], odcinek="E02", sezon="S08")
+
+filmy = Film
+serialy = Serial
+
+lista = [Karaiby, Spongebob, Dexter]
+get_movies = ["Karaiby", "Scooby_doo", "Dom_strachu"]
+get_series = ["Rodzinka", "Spongebob", "Dexter"]
+
+#Funkcja serial oraz film
+print(sorted(get_series))
+print(sorted(get_movies))
